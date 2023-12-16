d7ddf4980c9b585e3207e479251737942905ed45
Rishu kumar gupta <96177171+rkgupta7463@users.noreply.github.com>
2023-12-16 10:13:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
-def text_img(image_path, max_new_tokens=50):  # Adjust max_new_tokens as needed
+def text_img(image_path, max_new_tokens=150):  # Adjust max_new_tokens as needed
     # Load processor and model
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
